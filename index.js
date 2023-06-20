f014944952afc7db9de281ca8ef328d9c06f4ad7
AugustoCSV <guty.vertolis@gmail.com>
2023-06-20 16:06:17 -0300

    feat: creating function Binario2Decimal

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
-function sum(a,b){
-    return a + b;
+const input = '0110';
+const result = fn(input);
+
+function fn(input){
+
+    let listaConvertida = []
+    let numeroConvertido = 0;
+
+    for(let j = 0; j < input.length; j++){
+        let numero = input[j]
+        if(numero === "0"){
+            listaConvertida.push(0)
+        }else if (numero === "1"){
+            listaConvertida.push(1)
+        }
+    }
+
+    for(let i = 0; i < listaConvertida.length; i++){
+
+        n = listaConvertida.length;
+        num = listaConvertida[i];
+        numeroConvertido += num *(2**(n-i-1));
+    }
+    return numeroConvertido;
 }
 
-console.log(sum(2,3))
+console.log(result); // 6
