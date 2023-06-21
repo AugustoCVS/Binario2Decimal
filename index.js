d2e41190446a0a2e7d95962fd971af2d87721a09
AugustoCSV <guty.vertolis@gmail.com>
2023-06-21 08:36:10 -0300

    feat: create front-end and validations

--- index.js
+++ index.js
@@ -1,27 +1,87 @@
-const input = '0110';
-const result = fn(input);
+const inputBinary = document.getElementById("input__binario");
+const btnConverter = document.getElementById("btn__converter");
+const resultDiv = document.getElementById("resultado");
+const errorDiv = document.getElementById("error");
 
-function fn(input){
+const ERR_EMPTY_STRING = 'Input string is empty.';
+const ERR_NOT_BINARY = 'Input string is not a binary number.';
 
-    let listaConvertida = []
-    let numeroConvertido = 0;
+function validateBinaryString(binaryString) {
+    if (binaryString === ''){
+        const errorEmpty = ERR_EMPTY_STRING;
+        addError(errorEmpty)
+        resultDiv.innerHTML = ""
+        return false;
+        // throw new Error(ERR_EMPTY_STRING);
+    } 
+    if (!/^[01]+$/.test(binaryString)) {
+        const errorNotBinary = ERR_NOT_BINARY;
+        addError(errorNotBinary)
+        resultDiv.innerHTML = ""
+        return false;
+        // throw new Error(ERR_NOT_BINARY)
+    };
 
-    for(let j = 0; j < input.length; j++){
-        let numero = input[j]
-        if(numero === "0"){
-            listaConvertida.push(0)
-        }else if (numero === "1"){
-            listaConvertida.push(1)
-        }
-    }
-
-    for(let i = 0; i < listaConvertida.length; i++){
-
-        n = listaConvertida.length;
-        num = listaConvertida[i];
-        numeroConvertido += num *(2**(n-i-1));
-    }
-    return numeroConvertido;
+    return true;
 }
 
-console.log(result); // 6
+function convertBinaryToDecimal(binaryString){
+    if (!validateBinaryString(binaryString)) {
+        return; 
+    }else{
+        let response = [...binaryString].reduce((acc, bit, index) => {
+            return acc + Number(bit) * Math.pow(2, binaryString.length - index - 1)
+        }, 0)
+        errorDiv.innerHTML = ``
+        addResponse(response);
+    }
+    
+}
+
+function addResponse(response){
+    resultDiv.innerHTML = `<p> O numero em decimal é: ${response} </p>`
+
+    addImgAccept();
+}
+
+function addError(error) {
+    errorDiv.innerHTML = `<p class="errorMessage">${error}</p>`;
+
+    addImgError()
+  }
+
+  function addImgError() {
+    const divResponsesImg = document.createElement("div");
+    divResponsesImg.classList.add("errorImg");
+    document.body.appendChild(divResponsesImg);
+
+    const imgElement = document.createElement("img");
+    imgElement.src = "./img/gatinhoTriste.jpg";
+    divResponsesImg.appendChild(imgElement);
+
+    const previousImg = document.querySelector(".acceptImg");
+    if (previousImg) {
+        previousImg.remove();
+    }
+}
+
+function addImgAccept() {
+    const divResponsesImg = document.createElement("div");
+    divResponsesImg.classList.add("acceptImg");
+    document.body.appendChild(divResponsesImg);
+
+    const imgElement = document.createElement("img");
+    imgElement.src = "./img/gatinhoFeliz.jpg";
+    divResponsesImg.appendChild(imgElement);
+
+    const previousImg = document.querySelector(".errorImg");
+    if (previousImg) {
+        previousImg.remove();
+    }
+}
+
+
+btnConverter.addEventListener('click', function() {
+    convertBinaryToDecimal(inputBinary.value);
+    inputBinary.value = ""
+});
